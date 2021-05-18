8e9876b5700fa6b92c3e4ac465027653c3dfdc4f
Nozomi_Takahashi <nozo61118redhot@gmail.com>
2021-05-19 00:22:36 +0900

    welcome.js => welcome.jsx

--- helloworld/src/Article.js
+++ helloworld/src/Article.js
@@ -2,7 +2,7 @@ import { React } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './Article.scss';
-import Welcome from "./welcome";
+import Welcome from "./welcome.jsx";
 import Gobou from "./articles/Gobou";
 import Blueberry from "./articles/Blueberry";
 
