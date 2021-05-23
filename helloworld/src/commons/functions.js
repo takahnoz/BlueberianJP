77119255ab1d8bc413478752d389cc1c6fa8eae5
takahnoz <nozo61118redhot@gmail.com>
2021-05-23 15:46:57 +0900

    [Functions bug fix]

--- helloworld/src/commons/functions.js
+++ helloworld/src/commons/functions.js
@@ -1,4 +1,4 @@
-export const createListDomForTags = (effects_list) => {
+const createListDomForTags = (effects_list) => {
     let tag_list_dom = [];
     for (let i in effects_list) {
         tag_list_dom.push(<div className="Article_tag" key={i.toString()}>{effects_list[i]}</div>);
