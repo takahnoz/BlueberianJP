ef2e9b751e0416c2a0306fc2a96ad51130062208
TAKAHASHI NOZOMI <nozo61118redhot@gmail.com>
2021-05-26 18:09:23 +0900

    ファイル名変更

---
+++ helloworld/src/commons/Functions.js
@@ -0,0 +1,11 @@
+const createListDomForTags = (effects_list) => {
+    let tag_list_dom = [];
+    for (let i in effects_list) {
+        tag_list_dom.push(<div className="Article_tag" key={i.toString()}>{effects_list[i]}</div>);
+    }
+    return tag_list_dom;
+}
+
+const functions = { createListDomForTags };
+
+export { functions };
