397e550971190314992367e6a7296c979674b3b6
takahnoz <nozo61118redhot@gmail.com>
2021-05-30 12:20:46 +0900

    [検索コンポーネント敲き作成]

---
+++ helloworld/src/search/search.jsx
@@ -0,0 +1,13 @@
+import { React } from 'react'
+import { Route, Switch, /* useLocation */ } from 'react-router-dom';
+
+import './Searchs.scss';
+
+const Search = (props) => {
+    return(
+        <div>
+        </div>
+    );
+}
+
+export default Search;
