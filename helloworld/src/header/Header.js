171c504aa0b3b6d52c954234444624ac48014755
takahnoz <nozo61118redhot@gmail.com>
2021-06-05 12:43:02 +0900

    [Search系コンポーネント削除][Headerコンポーネントに検索機能移植]

--- helloworld/src/header/Header.js
+++ helloworld/src/header/Header.js
@@ -1,7 +1,5 @@
-import React, {useState} from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 import './Header.scss';
-import About from '../about/Search.jsx';
 
 const sub_title = 'なんとなく食べ物を理解する';
 const main_title = 'ぶるベリアン(仮)';
@@ -9,18 +7,6 @@ const main_title = 'ぶるベリアン(仮)';
 
 const Header = (props) => {
   
-  const init_desc = '調べたい食べ物を入力してくやさい。';
-  const keyword_history = useHistory();
-  const [word, setWord] = useState(
-    {
-      input_word: 'hoge'
-    }
-  );
-
-  const handleHistory = (word) => {
-    keyword_history.push(word);
-  };
-
   return (
     <div className="Header_wrap">
       <div className="Header">
@@ -28,14 +14,6 @@ const Header = (props) => {
           <p className="Sub_title">{sub_title}</p>
           <h1 className="Main_title">{main_title}</h1>
         </section>
-      </div>
-      <div className="Search_area">
-        <form>
-          <textarea className='Search_form' placeholder={init_desc} onChange={(e) => setWord(e.target.value)} />
-          <button className='Search_submit' onClick={() => handleHistory('word')}>
-            検索
-          </button>
-        </form>
       </div>
     </div>
   );
--- helloworld/src/navgation/Navigation.js
+++ helloworld/src/navgation/Navigation.js
@@ -1,19 +1,35 @@
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useHistory, Link } from 'react-router-dom';
 import './Navigation.scss';
 
-function Navigation() {
+const Navigation = () => {
+  const init_desc = '調べたい食べ物を入力してくやさい。';
+  const keyword_history = useHistory();
+  const [word, setWord] = useState(
+    {
+      input_word: 'hoge'
+    }
+  );
+
+  const handleHistory = (word) => {
+    keyword_history.push(word);
+  };
+
   return (
     <div className="Navigation_wrap">
+      <div className="Search_area">
+        <textarea className='Search_form' placeholder={init_desc} onChange={(e) => setWord(e.target.value)} />
+      </div>
       <div className="Navigation_area">
-        <nav>
-          <ul>
-            <li>
-                <Link to="/">HOME</Link>
+        <nav className="Navin_menu">
+          <ul className="Ul_list">
+            <li className="List">
+              <Link to="/">HOME</Link>
             </li>
-            <li>
+            <li className="List">
               <Link to="/Articles/Gobou">ごぼう</Link>
             </li>
-            <li>
+            <li className="List">
               <Link to="/Articles/Blueberry">ブルーベリー</Link>
             </li>
           </ul>
