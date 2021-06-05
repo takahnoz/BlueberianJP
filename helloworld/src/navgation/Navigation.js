05ebbb9df6fae2816505eed1c4a439863870a649
takahnoz <nozo61118redhot@gmail.com>
2021-06-05 21:30:08 +0900

    [naviメニューのコンポーネント化]

--- helloworld/src/navgation/Navigation.js
+++ helloworld/src/navgation/Navigation.js
@@ -1,37 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import './Navigation.scss';
+
+const foods_list_dom = {
+  'ごぼう': '/Articles/Gobou',
+  'ブルーベリー': '/Articles/Blueberry'
+};
+
 
 const Navigation = () => {
   const init_desc = '調べたい食べ物を入力してくやさい。';
   const keyword_history = useHistory();
-  const [word, setWord] = useState(
-    {
-      input_word: 'hoge'
-    }
-  );
+  const [food_word, setFoodWord] = useState();
+  console.log(`input word is ${food_word}`);
 
-  const handleHistory = (word) => {
-    keyword_history.push(word);
-  };
+  const FoodlistDom = (food_name) => {
+      return(
+        Object.keys(foods_list_dom).map((key) =>
+          <li className="List" key={key}>
+            <Link to={foods_list_dom[key]}>{key}</Link>
+          </li>
+        )
+      )
+  }
 
   return (
     <div className="Navigation_wrap">
       <div className="Search_area">
-        <textarea className='Search_form' placeholder={init_desc} onChange={(e) => setWord(e.target.value)} />
+        <textarea className='Search_form' placeholder={init_desc} onChange={(e) => setFoodWord(e.target.value)} />
       </div>
       <div className="Navigation_area">
         <nav className="Navin_menu">
           <ul className="Ul_list">
-            <li className="List">
-              <Link to="/">HOME</Link>
-            </li>
-            <li className="List">
-              <Link to="/Articles/Gobou">ごぼう</Link>
-            </li>
-            <li className="List">
-              <Link to="/Articles/Blueberry">ブルーベリー</Link>
-            </li>
+            <FoodlistDom />
           </ul>
         </nav>
       </div>
