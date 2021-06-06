78a85bd9159b91a7d69c595d7da41ee458d283f4
takahnoz <nozo61118redhot@gmail.com>
2021-06-06 20:05:11 +0900

    [navメニューの文言、URLをdataset.jsonから参照するよう設定]

--- helloworld/src/navgation/Navigation.js
+++ helloworld/src/navgation/Navigation.js
@@ -1,24 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import './Navigation.scss';
-
-const foods_list_dom = {
-  'ごぼう': '/Articles/Gobou',
-  'ブルーベリー': '/Articles/Blueberry'
-};
-
+import { FoodDataIndex } from '../assets/dataset.json';
 
 const Navigation = () => {
   const init_desc = '調べたい食べ物を入力してくやさい。';
-  const keyword_history = useHistory();
   const [food_word, setFoodWord] = useState();
   console.log(`input word is ${food_word}`);
 
   const FoodlistDom = (food_name) => {
       return(
-        Object.keys(foods_list_dom).map((key) =>
+        Object.keys(FoodDataIndex).map((key) =>
           <li className="List" key={key}>
-            <Link to={foods_list_dom[key]}>{key}</Link>
+            <Link to={FoodDataIndex[key]["Url"]}>{FoodDataIndex[key]["JpName"]}</Link>
           </li>
         )
       )
