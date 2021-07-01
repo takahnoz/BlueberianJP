45402a59e694385f9c872c81923ca6f7719d71a1
takahnoz <nozo61118redhot@gmail.com>
2021-07-01 14:12:42 +0900

    [keyの位置変更]

--- helloworld/src/Article.js
+++ helloworld/src/Article.js
@@ -2,7 +2,6 @@ import { React } from 'react'
 import { Route, Switch, /* useLocation */ } from 'react-router-dom';
 
 import './Article.scss';
-import Welcome from "./welcome.js";
 import FoodTemplate from "./articles/FoodTemplate";
 import NotFound from "./notfound/NotFound";
 // import { FoodDataIndex } from '../assets/dataset.json';
--- helloworld/src/commons/Functions.js
+++ helloworld/src/commons/Functions.js
@@ -4,8 +4,8 @@ const createListDomForTags = (effects_list) => {
     let tag_list_dom = [];
     for (let i in effects_list) {
         tag_list_dom.push(
-            <Link className="Article_tag_area" to="/">
-                <div className="Article_tag" key={i.toString()}>{effects_list[i]}</div>
+            <Link className="Article_tag_area" key={i.toString()} to="/">
+                <div className="Article_tag" >{effects_list[i]}</div>
             </Link>
         );
     }
